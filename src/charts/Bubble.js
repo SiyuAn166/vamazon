2265f1806e1d4d751ada44ad5702f067b7277643
SiyuAn166 <siyu.an166@gmail.com>
2023-11-05 22:56:52 -0800

    update

--- src/charts/Bubble.js
+++ src/charts/Bubble.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useRef } from 'react';
 import * as d3 from 'd3';
-import Legend from './Legend';
 
 const D3BubbleChart = ({ data }) => {
     const width = 1000;
@@ -52,51 +51,39 @@ const D3BubbleChart = ({ data }) => {
             .on('mouseleave', onMouseLeave);
 
 
-        // const legend = svg
-        //     .append('g')
-        //     .attr('class', 'legend')
-        //     .attr('transform', 'translate(20, 20)');
+        const legend = svg
+            .append('g')
+            .attr('class', 'legend')
+            .attr('transform', 'translate(20, 20)');
 
-        // const legendColors = color.domain();
-        // const legendRectSize = 18;
-        // const legendSpacing = 4;
+        const legendColors = color.domain();
+        const legendRectSize = 18;
+        const legendSpacing = 4;
 
-        // const legendItems = legend
-        //     .selectAll('.legend-item')
-        //     .data(legendColors)
-        //     .enter()
-        //     .append('g')
-        //     .attr('class', 'legend-item')
-        //     .attr('transform', (d, i) => `translate(0, ${i * (legendRectSize + legendSpacing)})`);
+        const legendItems = legend
+            .selectAll('.legend-item')
+            .data(legendColors)
+            .enter()
+            .append('g')
+            .attr('class', 'legend-item')
+            .attr('cursor', 'pointer')
+            .attr('transform', (d, i) => `translate(0, ${i * (legendRectSize + legendSpacing)})`);
 
-        // legendItems
-        //     .append('rect')
-        //     .attr('width', legendRectSize)
-        //     .attr('height', legendRectSize)
-        //     .attr('color', d => color(d))
-        //     .style('fill', d => color(d))
-        //     .on('click', (d) => {
-        //         // console.log(d.target.attributes.color.value)
-        //         // let div = document.createElement('div');
-        //         // document.body.appendChild(div);
-        //         // ReactDOM.render(<ColorPicker />, div);
-        //         DialogAlert.open({
-        //             alertTip:"加载失败，是否重新加载?",
-        //             confirmText:'重新加载',
-        //             cancelCallbackFn:()=>{
-        //                 window.history.back();
-        //             },
-        //             confirmCallbackFn:()=>{
-        //                 console.log("confirm")
-        //             }
-        //         })
-        //     })
+        legendItems
+            .append('rect')
+            .attr('width', legendRectSize)
+            .attr('height', legendRectSize)
+            .attr('color', d => color(d))
+            .style('fill', d => color(d))
+            .on('click', (d) => {
+                
+            })
 
-        // legendItems
-        //     .append('text')
-        //     .attr('x', legendRectSize + legendSpacing)
-        //     .attr('y', legendRectSize - legendSpacing)
-        //     .text(d => d);
+        legendItems
+            .append('text')
+            .attr('x', legendRectSize + legendSpacing)
+            .attr('y', legendRectSize - legendSpacing)
+            .text(d => d);
 
         function onMouseEnter(event, d) {
             const textElement = d3.select(event.target);
@@ -156,7 +143,6 @@ const D3BubbleChart = ({ data }) => {
         <div style={{ width: '100%', position: 'relative' }} id='bubble'>
             <svg ref={svgRef} width="50%">
             </svg>
-            <Legend color={color}/>
         </div>
     );
 };
--- src/charts/BubbleText.js
+++ src/charts/BubbleText.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useRef } from 'react';
 import * as d3 from 'd3';
-import Legend from './Legend';
 const BubbleText = ({ data }) => {
     const svgRef = useRef();
 
@@ -44,38 +43,39 @@ const BubbleText = ({ data }) => {
             .on('mouseenter', onMouseEnter)
             .on('mouseleave', onMouseLeave);
 
-        // const legend = svg
-        //     .append('g')
-        //     .attr('class', 'legend')
-        //     .attr('transform', 'translate(20, 20)');
+        const legend = svg
+            .append('g')
+            .attr('class', 'legend')
+            .attr('transform', 'translate(20, 20)');
 
-        // const legendColors = color.domain();
-        // const legendRectSize = 18;
-        // const legendSpacing = 4;
+        const legendColors = color.domain();
+        const legendRectSize = 18;
+        const legendSpacing = 4;
 
-        // const legendItems = legend
-        //     .selectAll('.legend-item')
-        //     .data(legendColors)
-        //     .enter()
-        //     .append('g')
-        //     .attr('class', 'legend-item')
-        //     .attr('transform', (d, i) => `translate(0, ${i * (legendRectSize + legendSpacing)})`);
+        const legendItems = legend
+            .selectAll('.legend-item')
+            .data(legendColors)
+            .enter()
+            .append('g')
+            .attr('class', 'legend-item')
+            .attr('cursor', 'pointer')
+            .attr('transform', (d, i) => `translate(0, ${i * (legendRectSize + legendSpacing)})`);
 
-        // legendItems
-        //     .append('rect')
-        //     .attr('width', legendRectSize)
-        //     .attr('height', legendRectSize)
-        //     .attr('color', d => color(d))
-        //     .style('fill', d => color(d))
-        //     .on('click', (d) => {
-        //         console.log(d.target.attributes.color.value)
-        //     })
+        legendItems
+            .append('rect')
+            .attr('width', legendRectSize)
+            .attr('height', legendRectSize)
+            .attr('color', d => color(d))
+            .style('fill', d => color(d))
+            .on('click', (d) => {
+                console.log(d.target.attributes.color.value)
+            })
 
-        // legendItems
-        //     .append('text')
-        //     .attr('x', legendRectSize + legendSpacing)
-        //     .attr('y', legendRectSize - legendSpacing)
-        //     .text(d => d);
+        legendItems
+            .append('text')
+            .attr('x', legendRectSize + legendSpacing)
+            .attr('y', legendRectSize - legendSpacing)
+            .text(d => d);
 
         function onMouseEnter(event, d) {
 
@@ -138,7 +138,6 @@ const BubbleText = ({ data }) => {
                 ref={svgRef}
                 width="50%"
             ></svg>
-            <Legend color={color}/>
         </div>
     );
 }
